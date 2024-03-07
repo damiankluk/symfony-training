fd440064e5795d41d26b7e17309da7fcf6dbee91
Damian Kluk <damian.kluk@sabre.com>
2024-03-07 20:06:00 +0100

    Add php-cs-fixer config file to repo

--- app/src/Repository/DepartureRepositoryInterface.php
+++ app/src/Repository/DepartureRepositoryInterface.php
@@ -4,22 +4,10 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-
 interface DepartureRepositoryInterface
 {
     /**
      * @return array<array<string|int, string>>
-     *
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
      */
     public function findDeparturesByStartAndEndStop(string $startStopId, string $endStopName): array;
 }
